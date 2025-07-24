0bcc74578a8091bd9c4493054ad39198fedad3be
funkycoder36 <ifti80@yahoo.com>
2025-07-24 20:52:38 +0500

    Project1

---
+++ Project1.py
@@ -0,0 +1,10 @@
+#Print a Sentence
+print("Welcome to the world of programming")
+#Print a Number
+print("2")
+#Next line after print
+print("Hello World \n")
+#pass multiple argumnets for printing
+print("hello",5)
+#end argument of print state
+print("Welcome to ", end= "*")
